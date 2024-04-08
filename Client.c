987c90d8a2363b979e75c9232b1a0392d24cd6a2
Yejin427 <yejinlala427@naver.com>
2024-04-09 00:41:23 +0900

    commit server, client

--- Client.c
+++ Client.c
@@ -1,1 +1,157 @@
-// test for git commit and push
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<pthread.h>
+
+#define BUF_SIZE 100
+#define NAME_SIZE 20
+
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
+char map[5][5];
+char mymark = ' ';
+char yourmark = ' ';
+void error_handling(char* message);
+void* send_msg(void* arg);
+void* rcv_msg(void* arg);
+void print_map();
+int made_line(int r, int c);
+
+int main(int argc, char* argv[]) {
+    int sock;
+    struct sockaddr_in serv_addr;
+    pthread_t snd_thread, rcv_thread;
+
+    void* thread_return;
+    if(argc != 4) {
+        printf("Usage: %s <IP> <PORT> <NAME>\n", argv[0]);
+        exit(1);
+    }
+    sprintf(name, "[%s]", argv[3]);
+
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port = htons(atoi(argv[2]));
+
+    if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
+        error_handling("connect() error!");
+    }
+    //내 순서를 서버에서 부여받음
+    int turn = 0;
+    // 서버로부터 클라이언트 번호 받기
+    char clnt_num_msg[BUF_SIZE];
+    read(sock, clnt_num_msg, BUF_SIZE);
+    printf("%s", clnt_num_msg); // 클라이언트 번호 출력
+    sscanf(clnt_num_msg, "You are client #%d\n", &turn); // 클라이언트 번호 저장
+    //mark 부여
+    if(turn == 1) {
+        mymark = 'o';
+        yourmark = 'x';
+    } else {
+        mymark = 'x';
+        yourmark = 'o';
+    }
+    
+    //빙고판 초기화
+    for(int i=0;i<5;i++) {
+        for(int j=0;j<5;j++) {
+            map[i][j] = '.';
+        }
+    }
+    pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
+    pthread_create(&rcv_thread, NULL, rcv_msg, (void*)&sock);
+    pthread_join(snd_thread, &thread_return);
+    pthread_join(rcv_thread, &thread_return);
+    close(sock);
+}
+void* send_msg(void* arg) {
+    int sock = *((int*)arg);
+    char name_msg[NAME_SIZE + BUF_SIZE];
+
+    while(1) {
+        printf("표시할 행, 열을 입력하시오(0~4) ex)0 4\n");
+        fgets(msg, BUF_SIZE, stdin);    //msg : 0 1
+        //입력형식 잘못됨
+        int r = 0, c = 0;
+        if(sscanf(msg, "%d %d\n", &r, &c) != 2) {
+            printf("입력 형식이 잘못되었습니다.\n");
+            continue;
+        }
+        sprintf(name_msg, "%s %s", name, msg);  
+        map[r][c] = mymark;
+
+        if(made_line(r, c)) {   //내가 이겼음 (선이 먼저 만들어졌음)
+            //만약 끝났다면 0 1 E 아니면 0 1 형태로 server에 보고
+            int len = strlen(name_msg);
+            name_msg[len + 1] = 'E';
+        }
+        write(sock, name_msg, strlen(name_msg));    //server에 write
+    }
+    return NULL;
+}
+void* rcv_msg(void* arg) {
+    int sock = *((int*)arg);
+    char name_msg[NAME_SIZE + BUF_SIZE];
+
+    int str_len;
+    while(1) {
+        str_len = read(sock, name_msg, NAME_SIZE + BUF_SIZE - 1);
+        if(str_len == -1) {
+            return (void*) -1;  //read 끝남
+        }
+        //서버에서 에러메시지를 받은 것임
+        if(!strcmp(name_msg, "당신의 차례가 아닙니다.")) {
+            fputs(name_msg, stdout);
+            continue;
+        }
+
+        //서버에서 결과를 부여받음
+        if(!strcmp(name_msg, "당신이 졌습니다.") || !strcmp(name_msg, "당신이 이겼습니다.")) {
+            fputs(name_msg, stdout);
+            close(sock);
+            exit(0);
+        }
+        name_msg[str_len] = 0;
+        fputs(name_msg, stdout);
+        //좌표를 받은 것일 때 -> 받은 위치로 빙고를 채움
+        printf("r: %c, c: %c\n", name_msg[str_len - 4], name_msg[str_len - 2]);
+        int r = name_msg[str_len - 4] - '0';
+        int c = name_msg[str_len - 2] - '0';
+        if(map[r][c] == '.') {  //해당 위치가 상대방이 놓을 위치일때만 채움
+            map[r][c] = yourmark;
+        }
+        //변한 틱택토판 출력
+        print_map();
+    }
+    return NULL;
+}
+void error_handling(char* message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+int made_line(int r, int c) {
+    //가로선, 세로선
+    if(map[0][c] == mymark && map[1][c] == mymark && map[2][c] == mymark && map[3][c] == mymark && map[4][c] == mymark) return 1;
+    if(map[r][0] == mymark && map[r][1] == mymark && map[r][2] == mymark && map[r][3] == mymark && map[r][4] == mymark) return 1;
+    //대각선
+    if(r == c){
+        if(map[0][0] == mymark && map[1][1] == mymark && map[2][2] == mymark && map[3][3] == mymark && map[4][4] == mymark) return 1;
+    }
+    if(r + c == 4) {
+        if(map[0][4] == mymark && map[1][3] == mymark && map[2][2] == mymark && map[3][1] == mymark && map[4][0] == mymark) return 1;
+    }
+    return 0;
+}
+void print_map() {
+    for(int i=0;i<5;i++) {
+        for(int j=0;j<5;j++) {
+            printf("%c ", map[i][j]);
+        }
+        printf("\n");
+    }
+}
--- Server.c
+++ Server.c
@@ -1,1 +1,123 @@
-// test for git commit and push
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<pthread.h>
+
+#define BUF_SIZE 100
+#define MAX_CLNT 256
+
+int clnt_cnt = 0;
+int clnt_socks[MAX_CLNT];
+const char* win_msg = "당신이 이겼습니다.";
+const char* lose_msg = "당신이 졌습니다.";
+const char* turn_msg = "당신의 차례가 아닙니다.";
+pthread_mutex_t mutex;
+
+void error_handling(char* message);
+void *handle_clnt(void* arg);
+void send_msg(char* msg, int len);
+void send_resultmsg(char* msg, int len, int sock_num);
+int main(int argc, char* argv[]) {
+    int serv_sock, clnt_sock;
+    struct sockaddr_in serv_adr, clnt_adr;
+    int clnt_adr_sz;
+    pthread_t t_id;
+    if(argc != 2) {
+        printf("Usage %s <PORT>", argv[0]);
+    }
+
+    pthread_mutex_init(&mutex, NULL);
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;  //tcpip 통신
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);   //내가 사용할 수 있는 ip 사용
+    serv_adr.sin_port = htons(atoi(argv[1]));   //port 저장
+
+    if(bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1){
+        error_handling("bind() error");
+    }
+    if(listen(serv_sock, 5) == -1) {
+        error_handling("listen() error");
+    }
+    while(1) {
+        clnt_adr_sz = sizeof(clnt_adr);
+        clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+        pthread_mutex_lock(&mutex);
+        clnt_socks[clnt_cnt++] = clnt_sock;
+        pthread_mutex_unlock(&mutex);
+        // 클라이언트에게 클라이언트 번호 보내기
+        char clnt_num_msg[BUF_SIZE];
+        sprintf(clnt_num_msg, "You are client #%d\n", clnt_cnt);
+        write(clnt_sock, clnt_num_msg, strlen(clnt_num_msg));
+        
+        pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
+        pthread_detach(t_id);
+        printf("Connected client IP: %s\n", inet_ntoa(clnt_adr.sin_addr));
+    }
+    close(serv_sock);
+    return 0;
+}
+void *handle_clnt(void* arg) {
+    int clnt_sock = *((int*)arg);
+    int str_len = 0, i;
+    char msg[BUF_SIZE];
+    int cur_turn = 0;
+
+    while((str_len = read(clnt_sock, msg, sizeof(msg))) != 0) {
+        send_msg(msg, str_len);//i행 j열
+        //만약 자신의 차례가 아니면 client에 알려줌
+        if(clnt_sock != clnt_socks[cur_turn]) {
+            send_msg(turn_msg, 40);
+            continue;
+        }
+        if(msg[str_len-1] == 'E') { //끝났음을 알림
+            send_resultmsg(win_msg, sizeof(win_msg), clnt_sock);
+
+            if(clnt_socks[0] == clnt_sock) {
+                send_resultmsg(lose_msg, sizeof(lose_msg), clnt_socks[1]);
+            } else {
+                send_resultmsg(lose_msg, sizeof(lose_msg), clnt_socks[0]);
+            }
+            break;
+        }
+        //순서를 넘김
+        if(cur_turn == 0) cur_turn = 1;
+        else cur_turn = 0;
+    }
+    pthread_mutex_lock(&mutex); //client 중간에 이탈할 수 있음!
+    for(i = 0; i < clnt_cnt; i++) {
+        if(clnt_sock == clnt_socks[i]) {
+            while(i++ < clnt_cnt-1) {
+                clnt_socks[i] = clnt_socks[i+1];
+            }
+            break;
+        }
+    }
+    clnt_cnt--;
+    pthread_mutex_unlock(&mutex);
+    close(clnt_sock);
+    return NULL;
+}
+void send_msg(char* msg, int len) {
+    int i;
+    pthread_mutex_lock(&mutex);
+    for(i=0;i<clnt_cnt;i++){    //접속 client가 같은 메시지 열람
+        write(clnt_socks[i], msg, len); 
+    }
+    pthread_mutex_unlock(&mutex);
+} 
+void error_handling(char* message) {
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+void send_resultmsg(char* msg, int len, int sock_num) {
+    pthread_mutex_lock(&mutex);
+    write(sock_num, msg, len);
+    pthread_mutex_unlock(&mutex);
+}
